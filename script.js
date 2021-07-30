3e9d2d899a296999781d5e44df21c60867005f89
Ramesh Reddy <60493999+ramesh181@users.noreply.github.com>
2021-07-30 20:41:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+function setFormMessage(formElement, type, message){
+    const messageElement = formElement.querySelector(".form_message");
+
+    messageElement.textContent = message;
+    messageElement.classList.remove("form_message_success", "form_message_error");
+    messageElement.classList.add(`form_message_${type}`);
+
+}
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const loginForm = document.querySelector("#login");
+    const createAccountForm = document.querySelector("#createAccount");
+
+    document.querySelector("#linkCreateAccount").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.add("form-hidden");
+        createAccountForm.classList.remove("form-hidden");
+
+    });
+
+    document.querySelector("#linkLogin").addEventListener("click", e => {
+        e.preventDefault();
+        loginForm.classList.remove("form-hidden");
+        createAccountForm.classList.add("form-hidden");
+    });
+
+    loginForm.addEventListener("submit", e => {
+        e.preventDefault();
+
+        // to perform Ajax/Fetch login
+        setFormMessage(loginForm, "error", "Invalid username/password combination");
+        setFormMessage(loginForm, "sucess", "You've successfuly Loged In!");
+
+        
+    });
+});
